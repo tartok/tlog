6aa4b26e37cae0a8aab1499ad7c98495eea746f9
Taras <tartok@ukr.net>
2024-02-12 15:02:54 +0200

    v1.0.7

--- stdout.go
+++ stdout.go
@@ -11,8 +11,32 @@ type (
 		outs   []func(p []byte) (n int, err error)
 		prefix []byte
 	}
+	FuncWriter struct {
+		w []func(p []byte) (n int, err error)
+	}
+	FuncReader struct {
+		r []func(p []byte) (n int, err error)
+	}
 )
 
+func NewFuncWriter(f ...func(p []byte) (n int, err error)) io.Writer {
+	return FuncWriter{w: f}
+}
+func (f FuncWriter) Write(p []byte) (n int, err error) {
+	for _, f := range f.w {
+		f(p)
+	}
+	return len(p), nil
+}
+func NewFuncReader(f ...func(p []byte) (n int, err error)) io.Reader {
+	return FuncReader{r: f}
+}
+func (f FuncReader) Read(p []byte) (n int, err error) {
+	for _, f := range f.r {
+		f(p)
+	}
+	return len(p), nil
+}
 func (o out) Write(p []byte) (n int, err error) {
 	if len(o.outs) == 0 {
 		return len(p), nil
